b2034ba258a37d92785852ee30723d4ed76f7996
N0life-code <fredericdurand972@gmail.com>
2021-08-25 15:10:55 +0200

    ajout du js

---
+++ main.js
@@ -0,0 +1,9 @@
+function films() {
+    let a = section.dataset.films; 
+    let spoilBatman = [["JOCKER MEURT"], ["BATMAN MEURT"], ["HARLEYQUEEN NE MEURT PAS"]];
+    let spoilKillBill = [["ELLE AIME TUER"], ["ELLE A PERDU SON ENFANT"], ["BILL C'EST SON EX"]];
+    if (a ) {
+        
+    }
+    
+}
